6c66853de086d9b9f211797ab8752e189f77f5aa
Gabriel Melo de Paula <gabrielmpaula@gmail.com>
2021-07-19 17:59:43 -0300

    New versions

--- src/main.py
+++ src/main.py
@@ -1,34 +1,9 @@
-import pydeck as pdk
 import streamlit as st
 import streamlit.components.v1 as components
-from streamlit_folium import folium_static
 from filometro import update_data, plot_map
 
 
-def create_map(data, lat, lon, zoom):
-    st.write(pdk.Deck(
-        map_style="mapbox://styles/mapbox/light-v9",
-        initial_view_state={
-            "latitude": lat,
-            "longitude": lon,
-            "zoom": zoom,
-        },
-        layers=[
-            pdk.Layer(
-                "HexagonLayer",
-                data=data,
-                get_position=["lon", "lat"],
-                radius=100,
-                elevation_scale=4,
-                elevation_range=[0, 1000],
-                pickable=True,
-                extruded=True,
-            ),
-        ]
-    ))
-
 counter = 0
-
 st.set_page_config(layout="wide")
 
 @st.cache(suppress_st_warning=True)
