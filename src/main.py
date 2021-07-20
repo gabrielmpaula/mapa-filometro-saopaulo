3e1abdf42eff834a425ded53d099844d0865d972
Gabriel Melo de Paula <gabrielmpaula@gmail.com>
2021-07-20 10:12:32 -0300

    Fixed set_page_config and added markdown

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ import streamlit.components.v1 as components
 from filometro import update_data, plot_map
 
 _counter = 0
-st.title('Mapa Filômetro SP')
 st.set_page_config(layout="wide")
+st.title('Mapa Filômetro SP')
 
 @st.cache(suppress_st_warning=True)
 def get_data():
@@ -13,6 +13,15 @@ def get_data():
 st.write("Localização dos postos de vacinação da Covid-19 na cidade de São Paulo")
 
 data = get_data()
+st.markdown(
+            """
+    <style>
+    canvas {
+    max-width: 100%!important;
+    height: auto!important;
+    </style>
+    """,
+    unsafe_allow_html=True)
 map = plot_map(data)
 components.html(map._repr_html_())
 
