2046af4d4b74b28a25c6675a15fdde9e332da234
Gabriel Melo de Paula <gabrielmpaula@gmail.com>
2021-07-21 01:08:57 -0300

    Update main.py
    Added updated time

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 import streamlit as st
+from datetime import datetime
 from filometro import update_data, plot_map, add_categorical_legend
 
-_counter = 0
+
 st.set_page_config(layout="wide", page_title='Mapa Filometrô SP', page_icon=':syringe:')
 st.title('Mapa Filômetro SP')
-
+_counter = 0
 @st.cache(suppress_st_warning=True)
 def get_data(_counter):
     data = update_data()
     map_clean, map_leg = plot_map(data)
-    return map_clean, map_leg
+    return map_clean, map_leg, datetime.now().date
+
+map_clean, map_leg, update_time = get_data(_counter)
 
 st.write("Localização dos postos de vacinação da Covid-19 na cidade de São Paulo")
 
@@ -22,9 +25,9 @@ if col1.button('Atualizar'):
     _counter += 1
 legend = col2.checkbox('Legenda', value=True)
 
-map_clean, map_leg = get_data(_counter)
-
 if legend:
     st.markdown(map_leg, unsafe_allow_html=True)
 else:
     st.markdown(map_clean, unsafe_allow_html=True)
+
+st.markdown(f'Última atualização {update_time.strftime("%d-%m-%Y")}')
